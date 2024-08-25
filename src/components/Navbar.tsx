53eab5375c761fbbd0e125aad6738c5eaf72df77
Azmal <azmalgazi8@gmail.com>
2024-08-25 18:32:34 +0600

    redux basic setup done and navabr created

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,82 @@
+import { Link, NavLink } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { Drawer } from "@material-tailwind/react";
+import { useState } from "react";
+import { HiOutlineShoppingBag } from "react-icons/hi2";
+import ProfileMenu from "./ProfileMenu";
+import { useAppSelector } from "../Redux/hooks";
+import { selectCurrentUser } from "../Redux/features/authSlice";
 
 const Navbar = () => {
-    return (
-        <div>
-            <h1>Navbar </h1>
+  const [open, setOpen] = useState(false);
+
+  const openDrawer = () => setOpen(true);
+  const closeDrawer = () => setOpen(false);
+
+  const user = useAppSelector(selectCurrentUser);
+
+  return (
+    <div className="flex justify-between items-center bg-primary py-3 px-10 md:px-10 lg:px-20 shadow-xl sticky top-0 z-20">
+      <img src="" className="w-[180px] md:w-[170px] lg:w-[230px]" />
+
+      <div className="hidden md:hidden lg:flex gap-7 justify-center items-center text-lg font-bold">
+        <NavLink to={"/"}>Home</NavLink>
+        <NavLink to={"/about-us"}>About Us</NavLink>
+        <NavLink to={"/contact-us"}>Contact Us</NavLink>
+
+        {user ? (
+          <div>
+            <ProfileMenu />
+          </div>
+        ) : (
+          <Link to={"/login"}>
+            <button className="cursor-pointer transition-all bg-button text-white px-4 py-1 rounded-lg border-blue-600 border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px] active:border-b-[2px] active:brightness-90 active:translate-y-[2px]">
+              Login
+            </button>
+          </Link>
+        )}
+      </div>
+
+      <div className="flex md:flex lg:hidden">
+        <GiHamburgerMenu
+          size={"20"}
+          className="cursor-pointer"
+          onClick={openDrawer}
+        />
+      </div>
+
+      <Drawer
+        open={open}
+        onClose={closeDrawer}
+        className="p-4 w-[180px]"
+        placeholder={undefined}
+        onPointerEnterCapture={undefined}
+        onPointerLeaveCapture={undefined}
+      >
+        <div className="mb-6 flex flex-col items-center justify-between">
+          <img src="" className="w-[150px] mb-7" />
+
+          <div className="flex flex-col gap-4 text-center">
+            <NavLink onClick={closeDrawer} to={"/"}>
+              Home
+            </NavLink>
+
+            <NavLink onClick={closeDrawer} to={"/products"}>
+              Products
+            </NavLink>
+
+            <NavLink onClick={closeDrawer} to={"/management"}>
+              Management
+            </NavLink>
+
+            <NavLink onClick={closeDrawer} to={"/cart"}>
+              <HiOutlineShoppingBag size={"30"} />
+            </NavLink>
+          </div>
         </div>
-    );
+      </Drawer>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ThemeProvider } from "@material-tailwind/react";
+import { Provider } from "react-redux";
+import { RouterProvider } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import router from "./routes/routes.tsx";
+import { store } from "./Redux/store.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <ThemeProvider>
+        <Toaster />
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </Provider>
+  </React.StrictMode>
+);
